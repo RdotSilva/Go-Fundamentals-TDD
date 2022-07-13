b18fc4eb586f08ec32a0f29e5ab7bad6595d5f94
RdotSilva <RyandotSilva@gmail.com>
2022-07-13 17:34:43 -0400

    Refactor Repeat function

--- iteration/iteration.go
+++ iteration/iteration.go
@@ -1,9 +1,12 @@
 package iteration
 
+const repeatCount = 5
+
+// Take a character and repeat it N number of times to create a new string of repeated characters
 func Repeat(character string) string {
 	var repeated string
-	for i := 0; i < 5; i++ {
-		repeated = repeated + character
+	for i := 0; i < repeatCount; i++ {
+		repeated += character
 	}
 	return repeated
 }
