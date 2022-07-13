290fd349f1f7571d56727da6ef00e0bc7f9d7ef5
RdotSilva <RyandotSilva@gmail.com>
2022-07-13 17:39:19 -0400

    Refactor Repeat to take in count

--- iteration/iteration.go
+++ iteration/iteration.go
@@ -1,9 +1,7 @@
 package iteration
 
-const repeatCount = 5
-
 // Take a character and repeat it N number of times to create a new string of repeated characters
-func Repeat(character string) string {
+func Repeat(character string, repeatCount int) string {
 	var repeated string
 	for i := 0; i < repeatCount; i++ {
 		repeated += character
