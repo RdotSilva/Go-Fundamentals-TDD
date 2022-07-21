1ea8d1b9402f1f3c5d69f2373d2e1c4a42b63361
RdotSilva <RyandotSilva@gmail.com>
2022-07-21 20:53:44 +0100

    Add Rectangle struct

--- structs/structs.go
+++ structs/structs.go
@@ -1,11 +1,19 @@
 package structs
 
+// Rectangle has the dimensions of a rectangle.
+type Rectangle struct {
+	Width  float64
+	Height float64
+}
+
 // Calculate the perimeter of a square/rectangle
-func Perimeter(width float64, height float64) float64 {
-	return 2 * (width + height)
+func Perimeter(rectangle Rectangle) float64 {
+	return 2 * (rectangle.Width + rectangle.Height)
 }
 
 // Calculate the area of a square/rectangle
-func Area(width float64, height float64) float64 {
-	return width * height
+func Area(rectangle Rectangle) float64 {
+	return rectangle.Width * rectangle.Height
 }
+
+
