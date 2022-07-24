e20cf54953d42cd16849ca53731a053d88ae3b88
RdotSilva <RyandotSilva@gmail.com>
2022-07-24 19:46:24 +0100

    Remove old unused tests

--- structs/structs_test.go
+++ structs/structs_test.go
@@ -14,30 +14,13 @@ func TestPerimeter(t *testing.T) {
 
 func TestArea(t *testing.T) {
 
-	checkArea := func(t testing.TB, shape Shape, want float64) {
-		t.Helper()
-		got := shape.Area()
-		if got != want {
-			t.Errorf("got %g want %g", got, want)
-		}
-	}
-
-	t.Run("rectangles", func(t *testing.T) {
-		rectangle := Rectangle{12, 6}
-		checkArea(t, rectangle, 72.0)
-	})
-
-	t.Run("circles", func(t *testing.T) {
-		circle := Circle{10}
-		checkArea(t, circle, 314.1592653589793)
-	})
-
 	areaTests := []struct {
 		shape Shape
 		want  float64
 	}{
 		{Rectangle{12, 6}, 72.0},
 		{Circle{10}, 314.1592653589793},
+		{Triangle{12, 6}, 36.0},
 	}
 
 	for _, tt := range areaTests {
@@ -48,3 +31,4 @@ func TestArea(t *testing.T) {
 	}
 
 }
+
